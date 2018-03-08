6fca11efbfcd10a041be2e1c87fe687d6d098ee8
Jérémy Carrier <jeremy@sogilis.com>
2018-03-08 17:33:38 +0100

    skeleton;

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 'use strict'
+const {BaseKonnector} = require('cozy-konnector-libs')
+const bluebird = require('bluebird')
 
+module.export = new BaseKonnector(start)
+
+function start (fields) {
+  return login()
+  .then(parseAccounts)
+  .then(saveAccounts)
+  .then(comptes =>
+    bluebird.each(comptes, compte => {
+      return fetchOperations(compte)
+        .then(operations => saveOperations(compte, operations))
+    })
+  )
+  .then(getDocuments)
+}
+
+function nop () {
+  console.log('nop')
+  return Promise.resolve([true])
+}
+
+const login = nop
+const parseAccounts = nop
+const saveAccounts = nop
+const fetchOperations = nop
+const saveOperations = nop
+const getDocuments = nop
