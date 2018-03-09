f737ea4f4ae905ad7aa2313834f1948a69b71816
Jérémy Carrier <jeremy@sogilis.com>
2018-03-09 12:19:31 +0100

    parseAccounts

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 'use strict'
-const {BaseKonnector} = require('cozy-konnector-libs')
+const {BaseKonnector, log, requestFactory} = require('cozy-konnector-libs')
 const connection = require('./connection')
 const bluebird = require('bluebird')
 
-const baseUrl = `https://espace-client.lanef.com/templates/logon`
+const baseUrl = `https://espace-client.lanef.com/templates`
 module.export = new BaseKonnector(start)
+
+const rq = requestFactory({
+  jar: true,
+  json: false,
+  cheerio: true
+})
 
 function start (fields) {
   return login(fields)
   .then(parseAccounts)
+  .then(fetchIBANs)
   .then(saveAccounts)
   .then(comptes =>
     bluebird.each(comptes, compte => {
@@ -24,8 +31,6 @@ function nop () {
   return Promise.resolve([true])
 }
 
-const parseAccounts = nop
-const saveAccounts = nop
 const fetchOperations = nop
 const saveOperations = nop
 const getDocuments = nop
@@ -35,10 +40,83 @@ function validateLogin (statusCode, $, json) {
 }
 
 function login (fields) {
+  log('info', 'Logging in')
   const page = 'logon.cfm'
   const population = {
     'USERID': fields.login,
     'STATIC': fields.password
   }
-  return connection.init(baseUrl, page, '#formSignon', population, validateLogin)
+  return connection.init(`${baseUrl}/logon`, page, '#formSignon', population, validateLogin)
+}
+
+function parseAccounts () {
+  log('info', 'Gettings accounts')
+
+  return rq(`${baseUrl}/landingPage/accountListWidget.cfm`)
+  .then($ => {
+    const accounts = Array.from($('#accountList li'))
+      .map(item => {
+        // NOTE It is possible that the user has given their account a pseudo
+        const label = $(item).children('div').eq(0).text().trim()
+        return {
+          label: label,
+          balance: parseAmount($(item).find('.pc-formatted-amount-value').text()),
+          type: ( label.match(/Parts Sociales/) ? 'liability' : 'bank' ),
+          number: $(item).attr('data-value')
+        }
+      })
+
+    return Promise.resolve(accounts.map(account => {
+      return {
+        institutionLabel: 'La Nef',
+        ...account
+      }
+    }))
+  })
+}
+
+function fetchIBANs (accounts) {
+  log('info', 'Fetching IBANs')
+
+  const params = {
+    accType: 'IBAN',
+    currencyCode: 'EUR',
+    page: '1',
+    viewMode: 'GRID'
+  }
+
+  return Promise.all(
+    accounts.map(account => {
+      if (account.type !== 'bank') {
+        // Only bank accounts can have an IBAN
+        return Promise.resolve(account)
+      } else {
+        return rq({
+          uri: `${baseUrl}/account/IBANDetail.cfm`,
+          method: 'POST',
+          form: {
+            AccNum: account.number,
+            ...params
+          }
+        }).then($ => {
+          const iban = $('.row').eq(12).children('div').eq(1).text().trim()
+          return Promise.resolve({
+            iban: iban,
+            ...account
+          })
+        })
+      }
+    })
+  ).then((accounts) => {
+    return Promise.resolve(accounts)
+  })
+}
+
+function parseAmount (amount) {
+  return parseFloat(amount.trim().replace('\xa0', '').replace(',', '.'))
+}
+
+function saveAccounts (accounts) {
+  console.log(accounts)
+  return accounts
 }
